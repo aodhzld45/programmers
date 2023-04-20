47cb56e35c259e0567cba2982f4848ff96f5a708
hyunseok <prking94@naver.com>
2023-04-20 10:11:17 +0900

    프로그래머스 음양 더하기

--- js/absolutesSign.js
+++ js/absolutesSign.js
@@ -2,7 +2,7 @@ function solution(absolutes, signs) {
     var answer = 0;
     var check = false; 
     
-    // 음양 더하기
+    // if(signs)
     
     /* absolutes.length와 signs.length같음 */
 
@@ -10,9 +10,12 @@ function solution(absolutes, signs) {
         if(signs[i] === true){
             console.log("양수 " + absolutes[i])
             answer += absolutes[i]
+            console.log(answer)
                                 
         }else {
             console.log("음수 " + absolutes[i])
+            answer += absolutes[i] * -1 // -1을 곱하여 음수로 처리
+
         }
         
 
